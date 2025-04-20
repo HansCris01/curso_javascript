fdf0244dec14af637fd4fcd6afc237ac23951fe8
HansCris01 <core512l@outlook.com>
2025-04-20 05:07:44 -0500

    subiendo nuevo contenido

--- clase1_parte1.js
+++ clase1_parte1.js
@@ -274,5 +274,105 @@ Less than or equal (a <= b) menor igual
 
 
 nos quedamos en 1:16:12
+*/
+
+//igualdad ejemplo:
+
+let numero = 23;
+let numero2 = 13;
+
+document.write(numero == numero2); //al comparar con == vamos a ver que nos da false porque no son iguales
+
+//igualdad desventaja caso 2 en caso si numero es texto
+
+let numero = "23";
+let numero2 = 23;
+
+document.write(numero == numero2) //si nos damos cuenta esta igualdad no es estricto
+
+
+//desigualdad 
+
+let numero = 23;
+let numero2 = 13;
+
+document.write(numero != numero2); //si hay desigualdad me dara true
+
+//identico este tipo de comparación es estricta 
+
+let numero = "23";
+let numero2 = 23;
+
+document.write(numero === numero2) /* aquí si te dice que es falso
+porque la comparación de estas dos variables uno es texto y el otro es numero */
+
+
+//no identico estricto
+
+let numero = "23";
+let numero2 = 23;
+
+document.write(numero !== numero2)/* aquí si te dice que es true
+porque la comparación de estas dos variables uno es texto y el otro es numero */
+
+//comparación de mayor
+
+let numero = 100;
+let numero2 = 50;
+
+document.write(numero > numero2); //te va decir true 
+
+//comparacion de mayor igual
+
+let numero = 100;
+let numero2 = 100;
+
+document.write(numero >= numero2); //te va decir true 
+
+//comparacion menor
+
+let numero = 100;
+let numero2 = 50;
+
+document.write(numero < numero2); //te va decir false 
+
+//comparacion menor igual 
+
+let numero = 100;
+let numero2 = 100;
+
+document.write(numero <= numero2); //te va decir true 
+
+
+/* Operador logico 
+
+  AND --> y
+  OR  --> o
+  NOT --> negación
 
 */
+
+let valor = true;
+let valor2 = true;
+
+let resultado = valor && valor2; //AND
+
+let resultado2 = valor || valor2; //OR
+
+let resultado3 = !valor; //NOT 
+
+document.write(resultado);
+document.write(resultado2);
+document.write(resultado3);
+
+
+//continuar 1:30:29
+
+
+
+
+
+
+
+
+
