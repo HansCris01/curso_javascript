ba9f1ed173c2ea94be5ba55aa7ac2e8a15ced915
Hans Luyo <38955181+HansCris01@users.noreply.github.com>
2025-04-24 15:29:01 -0500

    Update clase2.js
    for in vs for of

--- clase2.js
+++ clase2.js
@@ -8,4 +8,37 @@ for (animal in animales){
 	console.log(animal);
 }
 
+//Diferencia entre un for in vs un for of
+
+let planetas = ["Tierra", "Mercurio", "Venus", "Marte"];
+planetas.nombre = "Planetas";
+
+for (planeta in planetas){
+	console.log(planeta);
+}
+
+for (planeta of planetas){
+	console.log(planeta);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
