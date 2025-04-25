9195e23efce9a6d5f414b6562cfed361ef619419
HansCris01 <core512l@outlook.com>
2025-04-24 20:56:52 -0500

    subiendo curso

--- clase2.js
+++ clase2.js
@@ -46,9 +46,153 @@ for (let array in array2){
    Definicion y usos
    Formas de crear funciones (y llamarlas)
    return
-   Scope
    funciones flecha
 */
+
+function saludar(){
+	let respuesta = prompt("¡Hola! ¿Como fue tu día?");
+	if (respuesta == "bien"){
+		alert("me alegro");
+	} else {
+		alert("una pena")
+	}
+}
+
+
+saludar(); //aquí ejecuto la función
+
+//funcion con retorno
+
+function saludar() {
+  console.log('Hola');
+  return 'retornando la funcion';
+}
+
+let saludo = saludar();
+
+console.log(saludo);
+
+//funcion con parametros
+
+function suma(num1, num2){
+	let res = num1 + num2;
+	return res;
+}
+let resultado = suma(2,2);
+
+console.log(resultado);
+
+function saludar(nombre){
+	let frase = `¡Hola ${nombre}! ¿Como estas?`;
+	return frase;
+}
+
+let resultado = saludar("Hans");
+
+console.log(resultado); 
+
+
+//otra forma crear la función, pero funciona exac
+
+const saludar = function(nombre){
+	let frase = `¡Hola ${nombre}! ¿Como estás?`;
+	return frase;
+}
+
+let resultado = saludar("Hans");
+
+console.log(resultado);
+
+//Otra forma de crear funcion
+
+const saludar = (nombre)=>{
+	let frase = `¡Hola ${nombre}! ¿Como estás?`;
+	return frase;
+}
+
+let resultado = saludar("Sara");
+console.log(resultado);
+
+/*
+POO --> Programacion orientada a objetos 
+
+_ Definicion
+_ Ejemplos
+_ Instanciacion
+
+Conceptos básicos de POO 
+
+_ clase
+_ objeto
+_ atributo
+_ metodo
+_ constructor
+
+Caracteristica de la POO
+_ abstraccion
+_ modularidad
+_ jerarquia
+
+Otros conceptos de POO
+_ Polimorfismo
+_ Herencia
+
+Otros conceptos de POO
+_ Encapsulamiento
+
+*/
+
+//vamos a crear un objeto en un archivo nuevo de javascript
+
+
+class computadora {
+	constructor(procesador, memoriaRam, tarjetaVideo, memoriaAlmacenamiento){
+		this.procesador = procesador;
+		this.memoriaRam = memoriaRam;
+		this.tarjetaVideo = tarjetaVideo;
+		this.memoriaAlmacenamiento = memoriaAlmacenamiento;	
+		this.informacion = `Computadora ${procesador} con memoria ram de ${memoriaRam}, disco solido ${memoriaAlmacenamiento} y una tarjeta de video ${tarjetaVideo} de regalo`;
+	}
+}
+
+let ordenador = new computadora("Core I7","32GB DRR4","Nvidia Gforce", "1TB");
+//tambien podemos crear más computadoras con diferente información
+let ordenador2 = new computadora("rayzen","16GB DRR4","Ati Radeon", "2TB");
+
+
+console.log(ordenador);
+
+//acceder a las propiedades 
+
+console.log(ordenador.procesador);
+console.log(ordenador.informacion);
+
+console.log(ordenador2.informacion);
+
+//4:17:48 nos quedamos en objetos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
